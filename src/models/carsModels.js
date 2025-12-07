a8a05396de2e5b9f9d3836399bfdde7aede87c25
NicholasLeow123 <NICHOLASL.24@ichat.sp.edu.sg>
2025-12-07 20:56:08 +0800

    updated car models

--- src/models/carsModels.js
+++ src/models/carsModels.js
@@ -1,29 +1,19 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-// Get all cars
 async function getAllCars() {
   return prisma.cars.findMany({
     orderBy: { id: 'asc' }
   });
 }
 
-// Get one car by carsid (frontend identifier)
 async function getCarByCarsId(carsid) {
-  return prisma.cars.findUnique({
-    where: { carsid: carsid }
-  });
-}
-
-// Create a new car
-async function createCar(data) {
-  return prisma.cars.create({
-    data: data
+  return prisma.cars.findFirst({
+    where: { carsid }
   });
 }
 
 module.exports = {
   getAllCars,
-  getCarByCarsId,
-  createCar
+  getCarByCarsId
 };
